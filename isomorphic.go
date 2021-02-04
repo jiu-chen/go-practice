f714ed35f29cb07d647f7dbd33129c6c66fea535
second_chen <second_chen@trendmicro.com>
2021-02-04 19:05:13 +0800

    add

--- isomorphic.go
+++ isomorphic.go
@@ -1,19 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-//是否是同构数
+// 判断一个数 是否是同构数
 // 正整数n若是它平方数的尾部，则称n为同构数。
+
 // IsomorphicNum ...
 func IsomorphicNum(num int) bool {
-	a := num % 10
-	return Power(a, 2) == num
+	lens := len(strconv.Itoa(num))
+	r := Power(num, 2)
+	res := r % (Power(10, lens))
+	return res == num
 }
+
 func main() {
 	for i := 0; i < 10000; i++ {
 		if IsomorphicNum(i) {
 			fmt.Printf("%d 是同构数.", i)
 		}
 	}
-
+	fmt.Println()
 }
